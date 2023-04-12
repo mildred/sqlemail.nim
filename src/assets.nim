aae9a8ff926ef43c5bd87e64f3a9e9d667282765
Mildred Ki'Lya <mildred-pub.git@mildred.fr>
2023-04-13 01:06:44 +0200

    Use nim-embedfs

--- src/assets.nim
+++ src/assets.nim
@@ -1,8 +1,15 @@
-import options
+#import options
 
-const editor_js = staticRead("../assets/editor.js")
+#const editor_js = staticRead("../assets/editor.js")
+
+#proc getAsset*(file_path: string): Option[string] =
+#  case file_path
+#  of "assets/editor.js": return some(editor_js)
+#  else: none(string)
+
+import embedfs
+const data = embedDir("../assets")
 
 proc getAsset*(file_path: string): Option[string] =
-  case file_path
-  of "assets/editor.js": return some(editor_js)
-  else: none(string)
+  return data.get(file_path)
+
