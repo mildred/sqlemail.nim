e9223fd8f8bc9a65c2acc0673787bd8cc9c30ac3
Mildred Ki'Lya <mildred-pub.git@mildred.fr>
2023-05-01 23:08:03 +0200

    Update Vite config (all is forwarded to nim server except svelte app

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { existsSync } from 'node:fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +9,20 @@ export default defineConfig({
   plugins: [svelte()],
   server: {
     proxy: {
-      "/.well-known/disputatio/": "http://127.0.0.1:8080/"
+      "/.well-known/disputatio/": "http://127.0.0.1:8080/.well-known/disputatio/",
+      "/": {
+        target: "http://127.0.0.1:8080/",
+        bypass(req, res, opts) {
+          if (req.url.startsWith("/@vite/") || req.url.startsWith("/@fs/")) {
+            return req.url
+          }
+          let path = "app" + req.url
+          if (path.endsWith('/')) path += 'index.html'
+          if (existsSync(path)) {
+            return req.url
+          }
+        }
+      }
     }
   }
 })
